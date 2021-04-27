00e9bb99bb5b54ab341a594946a6f7abf8a8868b
ahmed-debbech <debbech.ahmed@gmail.com>
2021-04-28 00:42:03 +0100

    setup project

---
+++ chess_symfony/src/Entity/Game.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Game
+ *
+ * @ORM\Table(name="game")
+ * @ORM\Entity
+ */
+class Game
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ind", type="integer", nullable=false)
+     */
+    private $ind;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInd(): ?int
+    {
+        return $this->ind;
+    }
+
+    public function setInd(int $ind): self
+    {
+        $this->ind = $ind;
+
+        return $this;
+    }
+
+
+}
---
+++ chess_symfony/src/Entity/Move.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Move
+ *
+ * @ORM\Table(name="move", indexes={@ORM\Index(name="game", columns={"game"})})
+ * @ORM\Entity
+ */
+class Move
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="num", type="integer", nullable=false)
+     */
+    private $num;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="color", type="integer", nullable=false)
+     */
+    private $color;
+
+    /**
+     * @var \Game
+     *
+     * @ORM\ManyToOne(targetEntity="Game")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="game", referencedColumnName="id")
+     * })
+     */
+    private $game;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNum(): ?int
+    {
+        return $this->num;
+    }
+
+    public function setNum(int $num): self
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+
+    public function getColor(): ?int
+    {
+        return $this->color;
+    }
+
+    public function setColor(int $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getGame(): ?Game
+    {
+        return $this->game;
+    }
+
+    public function setGame(?Game $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+
+}
