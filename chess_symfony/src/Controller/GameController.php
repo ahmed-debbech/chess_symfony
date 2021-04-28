8c47ac106f8c8a67452b5567950e76f42a5af9c2
ahmed-debbech <debbech.ahmed@gmail.com>
2021-04-28 01:22:11 +0100

    create the joinning system

--- chess_symfony/src/Controller/GameController.php
+++ chess_symfony/src/Controller/GameController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Game;
 use App\Utils\Utilities;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class GameController extends AbstractController
 {
@@ -34,5 +37,28 @@ class GameController extends AbstractController
      */
     public function game($id){
         return $this->render('game/game.html.twig');
+    }
+    /**
+     * @Route("/join", name="join")
+     */
+    public function join(Request $request){
+        $form = $this->createFormBuilder()
+            ->add('id')
+            ->add('join', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            $game = $this->getDoctrine()->getRepository(Game::class)->find($data['id']);
+            if($game == NULL){
+                return new Response('no game found! press back');
+            }else{
+                dd($game);
+            }
+        }
+        return $this->render('game/join.html.twig', ['gamejoin'=> $form->createView()]);
     }
 }
---
+++ chess_symfony/src/Entity/Player.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Player
+ *
+ * @ORM\Table(name="player", indexes={@ORM\Index(name="game", columns={"game"})})
+ * @ORM\Entity
+ */
+class Player
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="color", type="integer", nullable=false)
+     */
+    private $color;
+
+    /**
+     * @var \Game
+     *
+     * @ORM\ManyToOne(targetEntity="Game")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="game", referencedColumnName="id")
+     * })
+     */
+    private $game;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getColor(): ?int
+    {
+        return $this->color;
+    }
+
+    public function setColor(int $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getGame(): ?Game
+    {
+        return $this->game;
+    }
+
+    public function setGame(?Game $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+
+}
